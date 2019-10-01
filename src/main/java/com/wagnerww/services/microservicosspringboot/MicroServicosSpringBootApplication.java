cd8a87b3218a7e50a4826a6d5e9cebb79caa56fb
wagner <wagnerricardonet@gmail.com>
2019-10-01 20:01:13 -0300

    chore: first commit

---
+++ src/main/java/com/wagnerww/services/microservicosspringboot/MicroServicosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerww.services.microservicosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicosSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnerww/services/microservicosspringboot/MicroServicosSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wagnerww.services.microservicosspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroServicosSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
